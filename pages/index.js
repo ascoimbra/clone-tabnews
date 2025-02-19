2c5dbfd76ceaa9f59ef7e68cc80e0f07042c057c
ascoimbra <arthur@coimbra.eng.br>
2025-02-19 23:46:13 +0000

    modifique a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Pai, eu amo você. Se você me ama, da uma risadinha!</h1>
+    return <h1>Eu amo você. Se você me ama, da uma risadinha!</h1>
 }
 export default Home;
